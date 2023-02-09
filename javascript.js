c084bee14df294fec96d87b42070861bb2c006a0
TheYonaz <117009178+TheYonaz@users.noreply.github.com>
2023-02-09 15:58:52 +0200

    removed / from js

--- javascript.js
+++ javascript.js
@@ -18,11 +18,11 @@ cards.forEach(card => randomColor(card))
 setInterval(() => cards.forEach(card => randomColor(card)), 5000);
 setInterval(() => buttons.forEach(button => randomColor(button)), 2500);
 //photo slider
-let pic ="/images/slider/pexels-mali-maeder-335692.jpg"
-let pic1  ="/images/slider/norbu-gyachung-wGMVs4y24x4-unsplash.jpg"
-let pic2 ="/images/slider/samuel-dixon-eatb7lScbCc-unsplash.jpg"
-let pic3 ="/images/slider/sonika-agarwal-QWf84ihd1og-unsplash.jpg" 
-let pic4 ="/images/slider/johanna-paula-perez-vinluan-p72_Bgx0Dbg-unsplash.jpg"
+let pic ="images/slider/pexels-mali-maeder-335692.jpg"
+let pic1  ="images/slider/norbu-gyachung-wGMVs4y24x4-unsplash.jpg"
+let pic2 ="images/slider/samuel-dixon-eatb7lScbCc-unsplash.jpg"
+let pic3 ="images/slider/sonika-agarwal-QWf84ihd1og-unsplash.jpg" 
+let pic4 ="images/slider/johanna-paula-perez-vinluan-p72_Bgx0Dbg-unsplash.jpg"
 let images = [pic,pic1,pic2,pic3,pic4]
 let start = 0
 function slide(){
