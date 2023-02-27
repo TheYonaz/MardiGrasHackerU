7ea3dfb4511880ab2446efd112bd8b1d11ee8199
TheYonaz <117009178+TheYonaz@users.noreply.github.com>
2023-02-27 16:44:13 +0200

    more pictures added slider with js

--- ASSETS/JS/media/media.js
+++ ASSETS/JS/media/media.js
@@ -1,45 +1,65 @@
 const picLibrary = document.querySelector(".picLibrary");
 const images = picLibrary.querySelectorAll("img");
-const bigPic = document.querySelector('.bigPic');
-const closebtn = document.querySelector('.closebtn');
-const leftBtn = document.querySelector('.leftBtn')
-const rightBtn = document.querySelector('.rightBtn')
-console.log(images);
+const bigPic = document.querySelector(".bigPic");
+const closebtn = document.querySelector(".closebtn");
+const leftBtn = document.querySelector(".leftBtn");
+const rightBtn = document.querySelector(".rightBtn");
+const imagesSrc = [
+  "./ASSETS/images/slider/cayetano-gil-T0tb5Olqkis-unsplash.jpg",
+  // "./ASSETS/images/slider/chalo-garcia-WD3MV8Mmr7U-unsplash.jpg",
+  "./ASSETS/images/slider/fili-santillan-nE_GZfPPhpQ-unsplash.jpg",
+  "./ASSETS/images/slider/johanna-paula-perez-vinluan-p72_Bgx0Dbg-unsplash.jpg",
+  "./ASSETS/images/slider/kankon-biswas-p4r46cTclno-unsplash.jpg",
+  "./ASSETS/images/slider/norbu-gyachung-wGMVs4y24x4-unsplash.jpg",
+  "./ASSETS/images/slider/pexels-mali-maeder-335692.jpg",
+  "./ASSETS/images/slider/pexels-mikhail-nilov-9391500.jpg",
+  "./ASSETS/images/slider/pexels-polina-kovaleva-8404700.jpg",
+  "./ASSETS/images/slider/samuel-dixon-eatb7lScbCc-unsplash.jpg",
+  "./ASSETS/images/slider/sander-dalhuisen-C1WGQahqHOw-unsplash.jpg",
+  "./ASSETS/images/slider/sonika-agarwal-QWf84ihd1og-unsplash.jpg",
+  "./ASSETS/images/slider/ugur-arpaci-U18V0ToioFU-unsplash.jpg",
+];
+// show images on load
+showImg();
+//add src to big pic on change show closebtn
 images.forEach((image) =>
-  image.addEventListener("click", () =>{ 
-    let src = image['src']
-    let toAdd = bigPic.querySelector('img')
-  toAdd['src'] = `${src}`
-  closebtn.style.opacity = '1'
-  }
-  )
+  image.addEventListener("click", () => {
+    let src = image["src"];
+    let toAdd = bigPic.querySelector("img");
+    toAdd["src"] = `${src}`;
+    closebtn.style.opacity = "1";
+  })
 );
-closebtn.addEventListener('click', () =>{
-  let toAdd = bigPic.querySelector('#showPic')
-  toAdd['src'] = ``
-  closebtn.style.opacity = '0'
+//close button
+closebtn.addEventListener("click", () => {
+  let toAdd = bigPic.querySelector("#showPic");
+  toAdd["src"] = ``;
+  closebtn.style.opacity = "0";
+});
+// on click change order of array and show the new order
+leftBtn.addEventListener("click", () => {
+  endToStart();
+  showImg();
+});
+rightBtn.addEventListener("click", () => {
+  startToEnd();
+  showImg();
+});
+//change the array order
+function endToStart(images) {
+  let toFirst = imagesSrc.pop();
+  imagesSrc.unshift(toFirst);
 }
-)
-rightBtn.addEventListener('click',()=>{
-  picLibrary.style.transform = 'translateX(-30%)';
-})
+function startToEnd(images) {
+  let toLast = imagesSrc.shift();
+  imagesSrc.push(toLast);
+}
 
-
-
-
-
-
-// function endToStart(images){
-//   let array = Array.from(images)
-//   let toFirst = array.pop()
-//   console.log(array)
-//   array.unshift(`${toFirst}`)
-//   console.log(array)
-// }
-// function startToEnd(images){
-//   let array = Array.from(images)
-//   let toLast = array.shift()
-//   array.push(`${toLast}`)
-// }
-// console.log(endToStart(images))
-
+function showImg() {
+  let i = 0;
+  for (img of images) {
+    if (i > imagesSrc.length) i = 0;
+    img["src"] = imagesSrc[i];
+    i++;
+  }
+}
