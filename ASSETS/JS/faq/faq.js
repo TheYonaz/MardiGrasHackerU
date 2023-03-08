82cf212e712b78d5f774035ad0bb873ee6c000ed
TheYonaz <117009178+TheYonaz@users.noreply.github.com>
2023-03-08 23:57:53 +0200

    added accordion

--- ASSETS/JS/faq/faq.js
+++ ASSETS/JS/faq/faq.js
@@ -1,13 +1,33 @@
-const part1 = document.querySelector('.part1')
-const span = document.querySelector('span')
+const part = document.querySelectorAll(".part");
+const span = document.querySelector("span");
+const part1Fall = document.querySelector(".part1Fall");
+const part2Fall = document.querySelector(".part2Fall");
+const part3Fall = document.querySelector(".part3Fall");
+const part4Fall = document.querySelector(".part4Fall");
+const part1 = document.querySelector("#part1");
+const part2 = document.querySelector("#part2");
+const part3 = document.querySelector("#part3");
+const part4 = document.querySelector("#part4");
 
-part1.addEventListener('click', ()=>{
-part1.style.translate ='95%';
+part.forEach((x) => {
+  x.addEventListener("click", (xy) => {
+    xy.currentTarget.classList.toggle("move-right");
+  });
+});
 
+span.addEventListener("click", () => {
+  part.classList.toggle("move-right");
+});
 
-})
-// span.addEventListener('click',() =>{
+function showText(name) {
+  if (name.style.display === "block") {
+    name.style.display = "none";
+  } else {
+    name.style.display = "block";
+  }
+}
 
-// console.log(span);
-// part1.style.translate ='0';
-// })
+part1.addEventListener("transitionend", () => showText(part1Fall));
+part2.addEventListener("transitionstart", () => showText(part2Fall));
+part3.addEventListener("transitionstart", () => showText(part3Fall));
+part4.addEventListener("transitionstart", () => showText(part4Fall));
